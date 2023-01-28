64b85cc2521a33dcdb7576b1b16f312f4a8e0e50
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-01-28 14:00:39 -0500

    get access to an Array with 100 memes by using fetch request, set a function that generate a random image url by clicking button, displayed that image on a screen

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+
 export default function Main(){
+    const [allMemeImages, setallMemeImages] = useState([])
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomtext: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    useEffect(()=> {
+        async function getMeme(){
+            const response = await fetch("https://api.imgflip.com/get_memes");
+            const data = await response.json();
+            setallMemeImages(data.data.memes)
+        }
+        getMeme()
+    },[])
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+    const getRandomMeme = () => {
+        const random = Math.floor(Math.random() * allMemeImages.length)
+        const url = allMemeImages[random].url
+        setMeme(prevState => ({
+            ...prevState,
+            randomImage: url
+        }))
+    }
     return(
         <main>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Top text"
@@ -12,10 +40,11 @@ export default function Main(){
                     placeholder="Bottom text"
                     name="bottomText"
                     className="inputField"/>
-                <button className="btn">Get a new meme image</button>
+                <button className="btn" 
+                onClick={getRandomMeme}>Get a new meme image</button>
+                <img src={meme.randomImage} alt="Meme" className="memeImage"/>
             </form>
             
         </main>
     )
 }
-// https://api.imgflip.com/get_memes  API
